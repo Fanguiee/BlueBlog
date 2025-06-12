efcb69ce7f0ba6e0bfdc127100c64a75b09269e0
Yajie Deng <yajie.deng@st.oth-regensburg.de>
2025-06-12 11:22:47 +0200

    .

--- src/main/java/com/jountain/demo/security/user/ShopUserDetailsService.java
+++ src/main/java/com/jountain/demo/security/user/ShopUserDetailsService.java
@@ -1,12 +1,23 @@
 package com.jountain.demo.security.user;
 
+import com.jountain.demo.model.User;
+import com.jountain.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class ShopUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+        return ShopUserDetails.buildUserDetails(user);
     }
 }
